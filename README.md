4d4ba4d376f60c88e22a4a9a24c4a7a9ccbfe157
zulfidly <zulfidly@gmail.com>
2023-07-30 19:35:41 +0800

    1

---
+++ README.md
@@ -0,0 +1,25 @@
+### v
+[Github](https://github.com/zulfidly/boilerplate-vue-vueuse-tailwind) 
+
+- ```npm init vue@latest```
+- ```cd v```
+- ```npm install```
+- ```npm run dev```
+- ```npm i @vueuse/core```
+
+
+- ```npm install -D tailwindcss postcss autoprefixer```
+- ```npx tailwindcss init -p```
+- see ```content: ["./src/**/*.{vue,js}"],``` in ```tailwind.config.js```
+
+```
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+```
+
+
+
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref } from 'vue'
+  import { useElementVisibility  } from '@vueuse/core'
+  import HelloWorld from './components/HelloWorld.vue'
+
+  const templateRef = ref(null)
+  const isElementVisible = useElementVisibility(templateRef)
+</script>
+
+<template>
+  <HelloWorld :msg="'this is a message'"/>
+  <main
+    ref="templateRef"
+    class="p-3 bg-slate-500" >
+    Testing 
+  </main>
+
+  <div class="h-[110vh]"></div>
+
+  <section class="fixed bottom-0 right-0">is visible: {{ isElementVisible }}</section>
+
+</template>
